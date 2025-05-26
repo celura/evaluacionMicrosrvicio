15bfe67c5d77b3bff3cc23049ec5182434ab01cf
celura <168040193+celura@users.noreply.github.com>
2025-05-25 19:49:48 -0500

    Update main.py

--- backend/evaluacionMicroservicio/main.py
+++ backend/evaluacionMicroservicio/main.py
@@ -16,7 +16,7 @@ def create_app():
     
     CORS(app, 
          resources={r"/*": {
-            "origins": ["https://calidad-servidor-front.vercel.app/"],
+            "origins": ["https://calidad-servidor-front.vercel.app"],
             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             "allow_headers": ["Content-Type", "Authorization"]
          }}, 
